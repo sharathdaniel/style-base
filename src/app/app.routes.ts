62dce0a507a00988f30787778e8727ad21c9bb4b
Sharath Daniel <5154064+sharathdaniel@users.noreply.github.com>
2026-01-02 18:18:30 +0530

    formatting updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'users',
-    loadChildren: () =>
-      import('./pages/users/users.routes').then((m) => m.routes),
+    loadChildren: () => import('./pages/users/users.routes').then((m) => m.routes),
   },
 ];
