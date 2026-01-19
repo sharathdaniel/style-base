7a4a96c6ecb52ccdb80f3854a375ee71db5e16b1
Sharath Daniel <5154064+sharathdaniel@users.noreply.github.com>
2026-01-20 00:08:07 +0530

    css grid demo

---
+++ src/app/pages/users/grid-demo/grid-demo.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-demo',
+  templateUrl: './grid-demo.html',
+  styleUrl: './grid-demo.scss',
+})
+export class GridDemo {}
