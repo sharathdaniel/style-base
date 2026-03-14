9413e6dad9c31a0225a3a02ac9181e780929a1a1
Sharath <5154064+sharathdaniel@users.noreply.github.com>
2026-03-15 00:42:01 +0530

    Implement sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IconRegistryComponent } from './icons/icon-registry.component';
 import { MainHeader } from './layout/main-header/main-header';
+import { Sidebar } from './layout/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: true,
-  imports: [RouterOutlet, IconRegistryComponent, MainHeader],
+  imports: [RouterOutlet, IconRegistryComponent, MainHeader, Sidebar],
   styleUrl: './app.scss',
 })
 export class App {
