30f0a7c4dc84c03631b182cc6a7ec279cbd76046
Sharath <5154064+sharathdaniel@users.noreply.github.com>
2026-04-07 07:03:11 +0530

    landing page updates

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
+  imports: [RouterLink],
 })
-export class Landing {}
+export class Landing {
+  currentYear = new Date().getFullYear();
+}
